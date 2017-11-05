e5e05f4a961fa1848fef6399ebd9530b45fda0f1
yuke <victor93@globo.com>
2017-11-05 20:11:30 -0200

    fator corrigido

--- sistema_de_assentos/src/Constantes.java
+++ sistema_de_assentos/src/Constantes.java
@@ -12,7 +12,7 @@ public class Constantes {
 		public static final int qtdade_assentos = 30;
 		public static final int qtdade_clientes = 50;
 		public static final int tempo_programa = 10000;//tempo de execução do programa (EM MILISSEGUNDOS )
-		public static final int fator = 10000;
+		public static final int fator = 1000;
 		public static volatile boolean fim_programa = false;
 		public static final String arquivo_saida = "/res/log.txt";
 		public static final String full_path = file_path + arquivo_saida;//"/res/log.txt";
