738d69a4d0378e21e1b3a3b2210eec0a4e55f061
yuke <victor93@globo.com>
2017-11-06 01:23:29 -0200

    modo console

--- sistema_de_assentos/src/Principal.java
+++ sistema_de_assentos/src/Principal.java
@@ -33,8 +33,8 @@ public class Principal {
 			System.out.println("\n----------------------------------------------------------------------------------------------------------------------------------------");
 			System.out.println("EXECUTANDO PROGRAMA E GERANDO O LOG DE SAÍDA, POR FAVOR AGUARDE..");
 			System.out.println("----------------------------------------------------------------------------------------------------------------------------------------");
-			 stream = new PrintStream(Constantes.full_path);       //SE DESCOMENTADO MOSTRATÁ A SAÍDA DAS THREADS EM UM ARQUIVO LOG
-			//stream = new PrintStream(console_stream);			 //SE DESCOMENTADO MOSTRARÁ A SAÍDA DAS THREADS NO PŔOPRIO CONSOLE DO ECLIPSE
+			// stream = new PrintStream(Constantes.full_path);       //SE DESCOMENTADO MOSTRATÁ A SAÍDA DAS THREADS EM UM ARQUIVO LOG
+			stream = new PrintStream(console_stream);			 //SE DESCOMENTADO MOSTRARÁ A SAÍDA DAS THREADS NO PŔOPRIO CONSOLE DO ECLIPSE
 			assentos = new Assento[Constantes.qtdade_assentos];
 			clientes = new Cliente[Constantes.qtdade_clientes];
 			// Runnable log_thread = new Salva_log(0,buffer);// thread log criada com id 0
@@ -65,7 +65,7 @@ public class Principal {
 			System.out.println(
 					"----------------------------------------------------------------------------------------------------------------------------------------");
 			System.out.println(
-					"PARA EXECUTAR NO MODO LOG DESCOMENTE A LINHA 28 E COMENTE A LINHA 29 DA CLASSE: Principal.java");
+					"PARA EXECUTAR NO MODO LOG DESCOMENTE A LINHA 36 E COMENTE A LINHA 37 DA CLASSE: Principal.java");
 			System.out.println(
 					"----------------------------------------------------------------------------------------------------------------------------------------");
 			System.out.println(
